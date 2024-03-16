6b490beda7ca1e4971aa1699e3ea10a8c8257247
khadija201 <khadija2qubra@gmail.com>
2024-03-16 06:23:34 +0500

    assigment 20 D

---
+++ app.js
@@ -0,0 +1,53 @@
+// nested.....
+var password = prompt("enter the password");
+if (password < "5"){
+    alert("ok")
+}
+
+// / Array
+var alphbetic=["i","j","k","l","m","n"]
+console.log(alphbetic[1]);
+// #
+var alphbetic=["i","j","k","l","m","n"]
+var arraylength = alphbetic.length;
+console.log(arraylength)
+// #
+var arryElement = ["firstElement"];
+myArray= "secondElement";
+alert(myArray)
+// #
+var pets=["cat","dog","fish","frog","donkey","lion","tiger","rat"]
+var pet= pets.slice(1,2,"duck")
+console.log(pet)
+// #
+var size =["S","M","XL","XXL","XXXL"] 
+size.pop();
+console.log(size)
+// #
+var size =["S","M","XL","XXL","XXXL"] 
+size.unshift(3,2,1)
+console.log(size);
+// #
+var size =["S","M","XL","XXL","XXXL"] 
+size.splice(3,0,"L")
+console.log(size);
+// #
+var size =["S","M","XL","XXL","XXXL"] 
+var regsize=size.slice(0,"L")
+console.log(regsize);
+// #
+var alphabet = ["h" , "i","j","k","l","m"]    
+alert (alphabet.push("y"))
+// #
+var alphabet = ["h" , "i","j","k","l","m"]    
+alert (alphabet.pop("m"))
+// #
+var alphabet=["h","i","j","k"]
+ alphabet.unshift()
+console.log(alphabet)
+
+// for loops
+
+
+
+
